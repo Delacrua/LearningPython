184cd4a1e9a6720c8634f271f28a276f65dd1c99
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-04-14 14:59:14 +0400

    Testig reset

--- fastPow.py
+++ fastPow.py
@@ -1,6 +1,7 @@
 def fast_pow(a, n):
     """
     The function takes an integer a and takes its power of n using fast recursive algorithm
+
     :param a: integer
     :param n: integer
     :return: integer
