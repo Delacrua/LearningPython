1ce7ef928c89fef7cbd4d08f061c24bea514d2b5
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-04-14 12:28:02 +0400

    Deleted tsrs.py

--- trsr.py
+++
@@ -1,7 +0,0 @@
-def array_diff(a, b):
-    s = ' '.join([str(i) for i in a])
-    for i in ''.join([str(i) for i in b]):
-        s.replace(i, '')
-    return [int(i) for i in s.split()]
-
-array_diff([1,2,2], [1])
