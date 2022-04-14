d1a1d6dbb465a8895cb15736eda407ccca47ad56
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-04-14 11:31:31 +0400

    added dockstring

--- hanoi.py
+++ hanoi.py
@@ -1,4 +1,12 @@
 def hanoi(n, i, k):
+    '''
+    The function visualizes the way to solve Hanoi Tower puzzle for 3 rods and n disks using recursion algorithm
+    :param n: number of disks on the first rod
+    :param i: the number of the stift where the original tower stands
+    :param k: the number of the disk, where tower must be moved
+    :return: None
+
+    '''
     if n == 1:
         print(f"Переложи диск 1 с стержня {i} на стержень {k}")
     else:
