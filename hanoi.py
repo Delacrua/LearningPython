b35e6a3a45f3aaf43e53bf4e20405175cfce3aee
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-04-14 12:02:24 +0400

    Updated docstring

--- hanoi.py
+++ hanoi.py
@@ -1,9 +1,9 @@
 def hanoi(n, i, k):
     '''
-    The function visualizes the way to solve Hanoi Tower puzzle for 3 rods and n disks using recursion algorithm
+    The function prints order of operations to solve Hanoi Tower puzzle for 3 rods and n disks using recursion algorithm
     :param n: number of disks on the starting rod
-    :param i: the number of the stift where the original tower stands
-    :param k: the number of the disk, where tower must be moved
+    :param i: the number of the rod on which the original tower stands
+    :param k: the number of the rod to which the tower must be moved
     :return: None
 
     '''
