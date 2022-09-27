d738d421fc989853aaae9abaddbb80a1ff72c988
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-09-27 12:06:38 +0400

    added a logger snippet

---
+++ CodeSnippets/basic_logger.py
@@ -0,0 +1,12 @@
+import logging
+logging.basicConfig(
+    filename='application.log',
+    level=logging.WARNING,
+    format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+logging.error("Some serious error occurred.")
+# [12:52:35] {<stdin>:1} ERROR - Some serious error occurred.
+logging.warning('Some warning.')
+# [12:52:35] {<stdin>:1} WARNING - Some warning.
