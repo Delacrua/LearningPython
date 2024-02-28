d030c58dbea76c6cfd5e4f26c17db1184541cb31
Uladzimir Fiodarau <ufiodarau@gmail.com>
2024-02-28 10:40:21 +0300

    added engines for sync and async connections, minor fixes to container composition

--- Tech/SQLAlchemy/src/database.py
+++ Tech/SQLAlchemy/src/database.py
@@ -1,16 +1,44 @@
+import asyncio
+import sys
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
 from config import settings
 
 
-engine = create_engine(
+sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
-    echo=True,
-    # pool_size=5,
-    # max_overflow=10,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
 )
 
-with engine.connect() as conn:
-    result = conn.execute("SELECT VERSION()")
-    print(f"res= {result}")
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+)
+
+
+def get_data_sync():
+    with sync_engine.connect() as conn:
+        result = conn.execute(text("SELECT VERSION()"))
+        print(f"res= {result.all()}")
+
+
+async def get_data_async():
+    async with async_engine.connect() as conn:
+        result = await conn.execute(text("SELECT 1,2,3 UNION SELECT 4,5,6"))
+        print(f"res= {result.all()}")
+
+
+if __name__ == "__main__":
+    get_data_sync()
+
+    # Only preform check if your code will run on non-windows environments.
+    if sys.platform == 'win32':
+        # Set the policy to prevent "Event loop is closed" error on Windows
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(get_data_async())
